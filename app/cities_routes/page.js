374f7b51675ed4cbae17e10568a1ddee904dbb10
Jesus Alejandro Ortiz Trejo <117122722+AlexOT03@users.noreply.github.com>
2024-10-29 10:49:26 -0600

    add the other page

--- app/cities_routes/page.js
+++ app/cities_routes/page.js
@@ -11,21 +11,21 @@ return (
         <h1 className="text-center text-2xl font-bold py-4">Seleccione el tipo de transporte</h1>
     </section>
     <section className='flex flex-col gap-8 md:flex-row md:gap-4'>
-        <a href="#" className="flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row md:max-w-xl hover:bg-gray-100">
+        <a href="/cities_routes/vehicle" className="flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row md:max-w-xl hover:bg-gray-100">
             <img className="object-cover w-full rounded-t-lg h-96 md:h-auto md:w-48 md:rounded-none md:rounded-s-lg" src="/docs/images/blog/image-4.jpg" alt=""/>
             <div className="flex flex-col justify-between p-4 leading-normal">
                 <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900">Combi</h5>
                 <p className="mb-3 font-normal text-gray-700 hidden">El transporte mas comun entre la cidudad</p>
             </div>
         </a>
-        <a href="#" className="flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row md:max-w-xl hover:bg-gray-100">
+        <a href="/cities_routes/vehicle" className="flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row md:max-w-xl hover:bg-gray-100">
             <img className="object-cover w-full rounded-t-lg h-96 md:h-auto md:w-48 md:rounded-none md:rounded-s-lg" src="/docs/images/blog/image-4.jpg" alt=""/>
             <div className="flex flex-col justify-between p-4 leading-normal">
                 <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900">Mini Bus</h5>
                 <p className="mb-3 font-normal text-gray-700 hidden">Vehiculos de tamaño mediano con mayor capacidad de personas</p>
             </div>
         </a>
-        <a href="#" className="flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row md:max-w-xl hover:bg-gray-100">
+        <a href="/cities_routes/vehicle" className="flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row md:max-w-xl hover:bg-gray-100">
             <img className="object-cover w-full rounded-t-lg h-96 md:h-auto md:w-48 md:rounded-none md:rounded-s-lg" src="/docs/images/blog/image-4.jpg" alt=""/>
             <div className="flex flex-col justify-between p-4 leading-normal">
                 <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900">Camion</h5>
