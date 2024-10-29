46b5c95af82d9bbde5c1e4d92e73337895f04ef5
Jesus Alejandro Ortiz Trejo <117122722+AlexOT03@users.noreply.github.com>
2024-10-29 10:30:13 -0600

    fix routes panel and improve the footer

--- app/cities_routes/page.js
+++ app/cities_routes/page.js
@@ -7,37 +7,94 @@ import Link from 'next/link';
 export default function Address() {
 return (
 <>
-    <div className='w-full text-sm text-center pb-4 pt-4'>
-        <h1 className='text-black text-xl font-30 font-bold md:text-md'>Selecciona tu tipo de transporte</h1>
-    </div>
-    <div className='w-full pt-10 pb-5 rounded-t-full grid p-2 gap-4 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3'>
-
-
-        <Link href="#" className='bg-[#6BC5E8] rounded-xl shadow-xl p-4 h-60 relative overflow-hidden'>
-        <p className='text-xl mb-4'>Ban</p>
-        <div className='w-full h-full flex justify-center items-center'>
-            <Image src="/icons/Ban.png" alt="Ban" width={160} height={160}
-                className="object-contain max-h-full sm:w-32 sm:h-32" priority />
+    <section>
+        <h1 className="text-center text-2xl font-bold py-4">Seleccione el tipo de transporte</h1>
+    </section>
+    <section className='flex flex-col gap-8 md:flex-row md:gap-4'>
+        <a href="#" className="flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row md:max-w-xl hover:bg-gray-100">
+            <img className="object-cover w-full rounded-t-lg h-96 md:h-auto md:w-48 md:rounded-none md:rounded-s-lg" src="/docs/images/blog/image-4.jpg" alt=""/>
+            <div className="flex flex-col justify-between p-4 leading-normal">
+                <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900">Combi</h5>
+                <p className="mb-3 font-normal text-gray-700 hidden">El transporte mas comun entre la cidudad</p>
+            </div>
+        </a>
+        <a href="#" className="flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row md:max-w-xl hover:bg-gray-100">
+            <img className="object-cover w-full rounded-t-lg h-96 md:h-auto md:w-48 md:rounded-none md:rounded-s-lg" src="/docs/images/blog/image-4.jpg" alt=""/>
+            <div className="flex flex-col justify-between p-4 leading-normal">
+                <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900">Mini Bus</h5>
+                <p className="mb-3 font-normal text-gray-700 hidden">Vehiculos de tamaño mediano con mayor capacidad de personas</p>
+            </div>
+        </a>
+        <a href="#" className="flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row md:max-w-xl hover:bg-gray-100">
+            <img className="object-cover w-full rounded-t-lg h-96 md:h-auto md:w-48 md:rounded-none md:rounded-s-lg" src="/docs/images/blog/image-4.jpg" alt=""/>
+            <div className="flex flex-col justify-between p-4 leading-normal">
+                <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900">Camion</h5>
+                <p className="mb-3 font-normal text-gray-700 hidden">Vehiculos de gran tamaño que circulas las principales rutas de la ciudad</p>
+            </div>
+        </a>
+    </section>
+    <section>
+        <h1 className="text-center text-2xl font-bold py-4">Seleccione el gremio de transporte</h1>
+    </section>
+    <section className='flex flex-col gap-4 items-center md:flex-row'>
+        <div className="w-full max-w-xs bg-white border border-gray-200 hover:bg-gray-100 rounded-lg shadow">
+            <a href='#'>
+            <div className="flex flex-col items-center">
+                <img className="w-24 h-24 mb-3 rounded-full shadow-lg" src="/docs/images/people/profile-picture-3.jpg" alt="Bonnie image"/>
+                <h5 className="mb-1 text-xl font-medium text-gray-900">ARVIT</h5>
+            </div>
+            </a>
         </div>
-        </Link>
-
-        <Link href="#" className='bg-[#6BC5E8] rounded-xl shadow-xl p-4 h-60 relative overflow-hidden'>
-        <p className='text-xl mb-4'>Camión liguero</p>
-        <div className='w-full h-full flex justify-center items-center'>
-            <Image src="/icons/Camionl.png" alt="Camión liguero" width={160} height={160}
-                className="object-contain max-h-full" priority />
+        <div className="w-full max-w-xs bg-white border border-gray-200 hover:bg-gray-100 rounded-lg shadow">
+            <a href='#'>
+            <div className="flex flex-col items-center">
+                <img className="w-24 h-24 mb-3 rounded-full shadow-lg" src="/docs/images/people/profile-picture-3.jpg" alt="Bonnie image"/>
+                <h5 className="mb-1 text-xl font-medium text-gray-900">UTPCAM</h5>
+            </div>
+            </a>
         </div>
-        </Link>
-
-        <Link href="#" className='bg-[#6BC5E8] rounded-xl shadow-xl p-4 h-60 relative overflow-hidden'>
-        <p className='text-xl mb-4'>Camión</p>
-        <div className='w-full h-full flex justify-center items-center'>
-            <Image src="/icons/Camion.png" alt="Camión" width={160} height={160} className="object-contain max-h-full"
-                priority />
+        <div className="w-full max-w-xs bg-white border border-gray-200 hover:bg-gray-100 rounded-lg shadow">
+            <a href='#'>
+            <div className="flex flex-col items-center">
+                <img className="w-24 h-24 mb-3 rounded-full shadow-lg" src="/docs/images/people/profile-picture-3.jpg" alt="Bonnie image"/>
+                <h5 className="mb-1 text-xl font-medium text-gray-900">VICOSENTRA</h5>
+            </div>
+            </a>
         </div>
-        </Link>
-
-    </div>
+        <div className="w-full max-w-xs bg-white border border-gray-200 hover:bg-gray-100 rounded-lg shadow">
+            <a href='#'>
+            <div className="flex flex-col items-center">
+                <img className="w-24 h-24 mb-3 rounded-full shadow-lg" src="/docs/images/people/profile-picture-3.jpg" alt="Bonnie image"/>
+                <h5 className="mb-1 text-xl font-medium text-gray-900">UTUCC</h5>
+            </div>
+            </a>
+        </div>
+        <div className="w-full max-w-xs bg-white border border-gray-200 hover:bg-gray-100 rounded-lg shadow">
+            <a href='#'>
+            <div className="flex flex-col items-center">
+                <img className="w-24 h-24 mb-3 rounded-full shadow-lg" src="/docs/images/people/profile-picture-3.jpg" alt="Bonnie image"/>
+                <h5 className="mb-1 text-xl font-medium text-gray-900">SETRATAB</h5>
+            </div>
+            </a>
+        </div>
+        <div className="w-full max-w-xs bg-white border border-gray-200 hover:bg-gray-100 rounded-lg shadow">
+            <a href='#'>
+            <div className="flex flex-col items-center">
+                <img className="w-24 h-24 mb-3 rounded-full shadow-lg" src="/docs/images/people/profile-picture-3.jpg" alt="Bonnie image"/>
+                <h5 className="mb-1 text-xl font-medium text-gray-900">GENESIS XXI</h5>
+            </div>
+            </a>
+        </div>
+        <div className="w-full max-w-xs bg-white border border-gray-200 hover:bg-gray-100 rounded-lg shadow">
+            <a href='#'>
+            <div className="flex flex-col items-center">
+                <img className="w-24 h-24 mb-3 rounded-full shadow-lg" src="/docs/images/people/profile-picture-3.jpg" alt="Bonnie image"/>
+                <h5 className="mb-1 text-xl font-medium text-gray-900">LINEA DORADA</h5>
+            </div>
+            </a>
+        </div>
+    </section>
+    <Footer />
 </>
 );
 }
