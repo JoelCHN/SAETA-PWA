73a1f9eabf59a1eb6bcc56817444299a543786f3
joelCHN <joelchable@outlook.com>
2024-11-10 00:43:14 -0600

    Revert "Merge branch 'fix/places'"
    This reverts commit 91398ddddf9fedcf08ec502f2a85f805c3681630, reversing
    changes made to bc785048c541cd8532d7c13e2672161f4d7210a9.

--- app/components/ui/CartPlace.js
+++ app/components/ui/CartPlace.js
@@ -1,27 +1,19 @@
-"use client"; // Indica que este es un componente cliente
-
 import React from "react";
-import { useRouter } from "next/navigation"; // Usa la nueva API de navegación
 
-const CartPlace = ({ id, title, age, imageUrl }) => {
-  const router = useRouter();
-
-  const handleClick = () => {
-    router.push(`../../places/${id}`);
-  };
-
+const CartPlace = ({ title = "Título del lugar", description = "Descripción breve del lugar." }) => {
   return (
-    <div
-      onClick={handleClick}
-      className="max-w-xs bg-white border border-gray-200 rounded-lg shadow-md overflow-hidden cursor-pointer"
-    >
-      <img className="w-full h-48 object-cover" src={imageUrl.src} alt={title} />
+    <div className="max-w-xs bg-white border border-gray-200 rounded-lg shadow-md overflow-hidden">
+      <img
+        className="w-full h-48 object-cover"
+        src="https://via.placeholder.com/150"
+        alt="Lugar turístico"
+      />
       <div className="p-4">
         <h2 className="text-lg font-semibold text-gray-800">{title}</h2>
-        <p className="text-gray-600 mt-2">{age}</p>
+        <p className="text-gray-600 mt-2">{description}</p>
       </div>
       <div className="p-4 flex justify-end">
-        <button className="bg-blue-500 text-white py-1 px-3 rounded">Conocer más</button>
+        <button className="bg-blue-500 text-white py-1 px-3 rounded hover:bg-blue-600">Ver más</button>
       </div>
     </div>
   );
