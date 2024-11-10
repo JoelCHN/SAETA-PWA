4e3a320e118bb570f98443dc39da895151deedd6
Jesus Alejandro Ortiz Trejo <117122722+AlexOT03@users.noreply.github.com>
2024-11-09 22:43:46 -0600

    fix

--- app/components/ui/Map.js
+++ app/components/ui/Map.js
@@ -35,13 +35,19 @@ export function Map({ ida, vuelta }) {
       const infoWindow = new InfoWindow();
 
       ida.forEach((element, index) => {
-        const busStopImg = document.createElement("img");
-        busStopImg.src = "/icons/bus-stop.png";
-        busStopImg.className = "w-8 h-8";
+        const parser = new DOMParser();
+        const pinSvgString =
+          '<svg  xmlns="http://www.w3.org/2000/svg"  width="24"  height="24"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round"  class="icon icon-tabler icons-tabler-outline icon-tabler-bus-stop"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M3 3m0 1a1 1 0 0 1 1 -1h2a1 1 0 0 1 1 1v4a1 1 0 0 1 -1 1h-2a1 1 0 0 1 -1 -1z" /><path d="M18 17m-2 0a2 2 0 1 0 4 0a2 2 0 1 0 -4 0" /><path d="M10 5h7c2.761 0 5 3.134 5 7v5h-2" /><path d="M16 17h-8" /><path d="M16 5l1.5 7h4.5" /><path d="M9.5 10h7.5" /><path d="M12 5v5" /><path d="M5 9v11" /></svg>';
+        const pinSvg = parser.parseFromString(
+          pinSvgString,
+          "image/svg+xml"
+        ).documentElement;
 
         const pin = new PinElement({
-          glyph: busStopImg,
-          scale: 1.5,
+          glyph: pinSvg,
+          glyphColor: "#ffffff",
+          background: "#16537e",
+          borderColor: "#2986cc",
         });
 
         const latLng = utm.convertUtmToLatLng(
@@ -69,13 +75,19 @@ export function Map({ ida, vuelta }) {
       });
 
       vuelta.forEach((element, index) => {
-        const busStopImg = document.createElement("img");
-        busStopImg.src = "/icons/bus-stop.png";
-        busStopImg.className = "w-8 h-8";
+        const parser = new DOMParser();
+        const pinSvgString =
+          '<svg  xmlns="http://www.w3.org/2000/svg"  width="24"  height="24"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round"  class="icon icon-tabler icons-tabler-outline icon-tabler-bus-stop"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M3 3m0 1a1 1 0 0 1 1 -1h2a1 1 0 0 1 1 1v4a1 1 0 0 1 -1 1h-2a1 1 0 0 1 -1 -1z" /><path d="M18 17m-2 0a2 2 0 1 0 4 0a2 2 0 1 0 -4 0" /><path d="M10 5h7c2.761 0 5 3.134 5 7v5h-2" /><path d="M16 17h-8" /><path d="M16 5l1.5 7h4.5" /><path d="M9.5 10h7.5" /><path d="M12 5v5" /><path d="M5 9v11" /></svg>';
+        const pinSvg = parser.parseFromString(
+          pinSvgString,
+          "image/svg+xml"
+        ).documentElement;
 
         const pin = new PinElement({
-          glyph: busStopImg,
-          scale: 1.5,
+          glyph: pinSvg,
+          glyphColor: "#ffffff",
+          background: "#990000",
+          borderColor: "#cc0000",
         });
 
         const latLng = utm.convertUtmToLatLng(
