d1a241280839918a1c1dad0f3356584dc7976ddf
Jesus Alejandro Ortiz Trejo <117122722+AlexOT03@users.noreply.github.com>
2024-11-11 09:48:59 -0600

    add image and code to show them

--- app/cities_routes/[id]/page.js
+++ app/cities_routes/[id]/page.js
@@ -31,6 +31,50 @@ export default async function Page({ params }) {
 
     return (
       <>
+        <nav className="flex mb-4" aria-label="Breadcrumb">
+          <ol className="inline-flex items-center space-x-1 md:space-x-2 rtl:space-x-reverse">
+            <li className="inline-flex items-center">
+              <a
+                href="/cities_routes"
+                className="inline-flex items-center text-sm font-medium text-gray-700 hover:text-blue-600"
+              >
+                <svg
+                  className="w-3 h-3 me-2.5"
+                  aria-hidden="true"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="currentColor"
+                  viewBox="0 0 20 20"
+                >
+                  <path d="m19.707 9.293-2-2-7-7a1 1 0 0 0-1.414 0l-7 7-2 2a1 1 0 0 0 1.414 1.414L2 10.414V18a2 2 0 0 0 2 2h3a1 1 0 0 0 1-1v-4a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1v4a1 1 0 0 0 1 1h3a2 2 0 0 0 2-2v-7.586l.293.293a1 1 0 0 0 1.414-1.414Z" />
+                </svg>
+                Rutas
+              </a>
+            </li>
+            <li aria-current="page">
+              <div class="flex items-center">
+                <svg
+                  class="rtl:rotate-180 w-3 h-3 text-gray-400 mx-1"
+                  aria-hidden="true"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 6 10"
+                >
+                  <path
+                    stroke="currentColor"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="2"
+                    d="m1 9 4-4-4-4"
+                  />
+                </svg>
+                <span class="ms-1 text-sm font-medium text-gray-500 md:ms-2">
+                  Detalles
+                </span>
+              </div>
+            </li>
+          </ol>
+        </nav>
+
         <section className="mb-4">
           <div className="max-w-7xl p-6 flex flex-col md:flex-row md:justify-between overflow-hidden bg-white border border-gray-200 rounded-lg shadow">
             <div className="order-2 md:order-1">
@@ -53,11 +97,15 @@ export default async function Page({ params }) {
             </div>
             <div className="order-1 mb-5 md:order-2 md:mb-0 flex items-center justify-center">
               <Image
-                className="aspect-square border"
-                src={routeDetails.vehicle_img}
+                className="aspect-[1111/512]"
+                src={
+                  routeDetails.agency === "ARVIT"
+                    ? "/combis/Combi_ARVIT.png"
+                    : "/combis/Combi_Vico.png"
+                }
                 alt={routeDetails.vehicle_type}
-                width={200}
-                height={200}
+                width={350}
+                height={350}
               />
             </div>
           </div>
--- app/cities_routes/page.js
+++ app/cities_routes/page.js
@@ -19,20 +19,47 @@ export default async function Address() {
 
     return (
       <>
-        <section className="w-full h-full flex flex-col items-center justify-center">
+        <nav className="flex mb-4" aria-label="Breadcrumb">
+          <ol className="inline-flex items-center space-x-1 md:space-x-2 rtl:space-x-reverse">
+            <li aria-current="page">
+              <div className="flex items-center text-gray-700">
+                <svg
+                  className="w-3 h-3 me-2.5"
+                  aria-hidden="true"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="currentColor"
+                  viewBox="0 0 20 20"
+                >
+                  <path d="m19.707 9.293-2-2-7-7a1 1 0 0 0-1.414 0l-7 7-2 2a1 1 0 0 0 1.414 1.414L2 10.414V18a2 2 0 0 0 2 2h3a1 1 0 0 0 1-1v-4a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1v4a1 1 0 0 0 1 1h3a2 2 0 0 0 2-2v-7.586l.293.293a1 1 0 0 0 1.414-1.414Z" />
+                </svg>
+                <span className="inline-flex items-center text-sm font-medium">
+                  Rutas
+                </span>
+              </div>
+            </li>
+          </ol>
+        </nav>
+
+        <section className="w-full h-full flex flex-col">
           <h1 className="text-xl uppercase font-bold mb-4">
             Rutas de transporte
           </h1>
-          <ul className="flex flex-col gap-4">
+          <ul className="w-full flex flex-col gap-4">
             {routes.map((route) => (
               <li key={route.id}>
                 <Link
                   href={`cities_routes/${route.id}`}
-                  className="flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row md:max-w-5xl hover:bg-gray-100"
+                  className="flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row md:max-w-7xl hover:bg-gray-100"
                 >
                   <Image
-                    src={route.vehicle_img}
-                    alt={route.vehicle_type}
+                    src={
+                      route.agency === "ARVIT"
+                        ? "/agency/Arvit_Icon.png"
+                        : "/agency/Vicosertra_Icon.png"
+                    }
+                    width={400}
+                    height={200}
+                    alt={route.agency}
                     className="object-cover w-full rounded-t-lg h-96 md:h-auto md:w-48 md:rounded-none md:rounded-s-lg"
                   />
                   <div className="flex flex-col justify-between p-4 leading-normal">
