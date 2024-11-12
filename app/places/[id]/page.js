bb5a00d89424bc5b8da6afb4c15ddb7f95fed3b6
joelCHN <joelchable@outlook.com>
2024-11-11 18:00:50 -0600

    Modificacion de lugares turisticas

--- app/places/[id]/page.js
+++ app/places/[id]/page.js
@@ -16,16 +16,33 @@ return <p>Lugar no encontrado</p>;
 
 return (
 <div className="p-6">
-    <a href="../places/"
-        className="cursor-pointer transition-transform transform hover:scale-105 inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-        <svg class="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
-            width="24" height="24" fill="none" viewBox="0 0 24 24">
-            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                d="M5 12h14M5 12l4-4m-4 4 4 4" />
-        </svg>
-
-
-    </a>
+    <nav className="flex mb-4" aria-label="Breadcrumb">
+        <ol className="inline-flex items-center space-x-1 md:space-x-2 rtl:space-x-reverse">
+            <li className="inline-flex items-center">
+                <a href="/places"
+                    className="inline-flex items-center text-sm font-medium text-gray-700 hover:text-blue-600">
+                    <svg className="flex-shrink-0 w-6 h-6 p-1 text-gray-500 transition duration-75 group-hover:text-gray-900"
+                        aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 18 20">
+                        <path
+                            d="M17 5.923A1 1 0 0 0 16 5h-3V4a4 4 0 1 0-8 0v1H2a1 1 0 0 0-1 .923L.086 17.846A2 2 0 0 0 2.08 20h13.84a2 2 0 0 0 1.994-2.153L17 5.923ZM7 9a1 1 0 0 1-2 0V7h2v2Zm0-5a2 2 0 1 1 4 0v1H7V4Zm6 5a1 1 0 1 1-2 0V7h2v2Z" />
+                    </svg>
+                    Lugares turísticos
+                </a>
+            </li>
+            <li aria-current="page">
+                <div class="flex items-center">
+                    <svg class="rtl:rotate-180 w-3 h-3 text-gray-400 mx-1" aria-hidden="true"
+                        xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
+                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                            d="m1 9 4-4-4-4" />
+                    </svg>
+                    <span class="ms-1 text-sm font-medium text-gray-500 md:ms-2">
+                        Detalles
+                    </span>
+                </div>
+            </li>
+        </ol>
+    </nav>
 
     <h1 className="text-2xl font-bold text-center mb-4">
         Detalle del lugar turístico: <span className="text-black">{place.title}</span>
--- app/places/page.js
+++ app/places/page.js
@@ -2,24 +2,38 @@ import CartPlace from '../components/ui/CartPlace';
 import PlacesData from '../data/placesdata/PlacesData';
 
 export default function PlacesPage() {
-  return (
-    <>
-      <h1 className="text-2xl font-bold text-black text-center mb-6">Lugares turísticos</h1>
-      <p className="text-xl text-gray-800 p-4">
+return (
+<>
+    <nav className="flex mb-4" aria-label="Breadcrumb">
+        <ol className="inline-flex items-center space-x-1 md:space-x-2 rtl:space-x-reverse">
+            <li aria-current="page">
+                <div className="flex items-center text-gray-700">
+                    <svg className="flex-shrink-0 w-6 h-6 p-1  text-gray-500 transition duration-75 group-hover:text-gray-900"
+                        aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 18 20">
+                        <path
+                            d="M17 5.923A1 1 0 0 0 16 5h-3V4a4 4 0 1 0-8 0v1H2a1 1 0 0 0-1 .923L.086 17.846A2 2 0 0 0 2.08 20h13.84a2 2 0 0 0 1.994-2.153L17 5.923ZM7 9a1 1 0 0 1-2 0V7h2v2Zm0-5a2 2 0 1 1 4 0v1H7V4Zm6 5a1 1 0 1 1-2 0V7h2v2Z" />
+                    </svg>
+                    <span className="inline-flex items-center text-sm font-medium">
+                        Lugares turísticos
+                    </span>
+                </div>
+            </li>
+        </ol>
+    </nav>
+
+
+    <h1 className="text-2xl font-bold text-black text-center mb-6 sm:text-3xl md:text-4xl lg:text-5xl">
+        Lugares turísticos
+    </h1>
+    <p className="text-lg text-gray-800 p-4 sm:text-base md:text-lg lg:text-xl">
         Conoce los lugares turísticos que se encuentran en la zona de Villahermosa, Tabasco
         <span className="font-bold"> ¡Tú decides a dónde ir!</span>
-      </p>
-      <div className="p-6 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+    </p>
+    <div className="p-6 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
         {PlacesData.map((place) => (
-          <CartPlace
-            key={place.id}
-            id={place.id}
-            title={place.title}
-            age={place.age}
-            imageUrl={place.imageUrl}  
-          />
+        <CartPlace key={place.id} id={place.id} title={place.title} age={place.age} imageUrl={place.imageUrl} />
         ))}
-      </div>
-    </>
-  );
+    </div>
+</>
+);
 }
