91cf15bbfd4683c28c9e8933b17302c7ed366aec
Jesus Alejandro Ortiz Trejo <117122722+AlexOT03@users.noreply.github.com>
2024-11-25 18:54:09 -0600

    fix carrusel

--- app/places/[id]/page.js
+++ app/places/[id]/page.js
@@ -1,5 +1,6 @@
 import PlacesData from "../../data/placesdata/PlacesData";
-import Image from "next/image";
+import { Carrusel } from "../../components/ui/Carrusel";
+import { LeafletMapSolo } from "../../components/ui/LeafletMapSolo";
 
 export default async function Details({ params }) {
   const id = (await params).id;
@@ -32,9 +33,9 @@ export default async function Details({ params }) {
             </a>
           </li>
           <li aria-current="page">
-            <div class="flex items-center">
+            <div className="flex items-center">
               <svg
-                class="rtl:rotate-180 w-3 h-3 text-gray-400 mx-1"
+                className="rtl:rotate-180 w-3 h-3 text-gray-400 mx-1"
                 aria-hidden="true"
                 xmlns="http://www.w3.org/2000/svg"
                 fill="none"
@@ -42,13 +43,13 @@ export default async function Details({ params }) {
               >
                 <path
                   stroke="currentColor"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="2"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
                   d="m1 9 4-4-4-4"
                 />
               </svg>
-              <span class="ms-1 text-sm font-medium text-gray-500 md:ms-2">
+              <span className="ms-1 text-sm font-medium text-gray-500 md:ms-2">
                 Detalles
               </span>
             </div>
@@ -57,77 +58,7 @@ export default async function Details({ params }) {
       </nav>
 
       <section className="mb-4 flex flex-col gap-3 md:flex-row md:gap-6">
-        <div
-          id="controls-carousel"
-          className="relative w-full md:w-1/2"
-          data-carousel="static"
-        >
-          <div className="relative h-56 overflow-hidden rounded-lg md:h-96 shadow">
-            {place.carrusel.map((img, index) => (
-              <div
-                key={index}
-                className="hidden duration-700 ease-in-out bg-gray-50"
-                data-carousel-item={index == 0 ? "active" : ""}
-              >
-                <Image
-                  src={img}
-                  alt={`Image ${index + 1}`}
-                  width={400}
-                  height={600}
-                  className="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
-                />
-              </div>
-            ))}
-          </div>
-          <button
-            type="button"
-            className="absolute top-0 start-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none"
-            data-carousel-prev
-          >
-            <span className="inline-flex items-center justify-center w-10 h-10 rounded-full bg-white/30 group-hover:bg-white/50 group-focus:ring-4 group-focus:ring-white group-focus:outline-none">
-              <svg
-                className="w-4 h-4 text-white rtl:rotate-180"
-                aria-hidden="true"
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 6 10"
-              >
-                <path
-                  stroke="currentColor"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="2"
-                  d="M5 1 1 5l4 4"
-                />
-              </svg>
-              <span className="sr-only">Previous</span>
-            </span>
-          </button>
-          <button
-            type="button"
-            className="absolute top-0 end-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none"
-            data-carousel-next
-          >
-            <span className="inline-flex items-center justify-center w-10 h-10 rounded-full bg-white/30 group-hover:bg-white/50 group-focus:ring-4 group-focus:ring-white group-focus:outline-none">
-              <svg
-                className="w-4 h-4 text-white rtl:rotate-180"
-                aria-hidden="true"
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 6 10"
-              >
-                <path
-                  stroke="currentColor"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="2"
-                  d="m1 9 4-4-4-4"
-                />
-              </svg>
-              <span className="sr-only">Next</span>
-            </span>
-          </button>
-        </div>
+        <Carrusel images={place.carrusel} />
         <div className="w-full md:w-1/2">
           <h1 className="text-4xl font-bold mt-4">{place.title}</h1>
           <div className="mt-1">
@@ -142,13 +73,21 @@ export default async function Details({ params }) {
           <p className="text-gray-700 mt-4">{place.description}</p>
         </div>
       </section>
-
+      <div className="w-full border my-4"></div>
       <section className="mb-4 flex flex-col gap-3 md:flex-row md:gap-6">
         <div className="w-full md:w-1/2">
           <h2 className="text-2xl font-bold">Historia</h2>
-          <p className="text-gray-700">{place.history}</p>
+          <p className="text-gray-700 mt-2">{place.history}</p>
         </div>
-        <div className="w-full md:w-1/2"></div>
+        <div className="w-full md:w-1/2">
+          <h2 className="text-2xl font-bold">Ubicación</h2>
+          <LeafletMapSolo
+            place_position={[place.location.lat, place.location.lng]}
+            place_name={place.title}
+            place_image={place.imageUrl.src}
+            className="mt-2"
+          />
+        </div>
       </section>
     </>
   );
