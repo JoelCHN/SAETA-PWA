0443eb9ee8b9735ba3e4eeacadeb48be8bac40dc
Jesus Alejandro Ortiz Trejo <117122722+AlexOT03@users.noreply.github.com>
2024-11-26 15:11:13 -0600

    fix styles

--- app/components/ui/CardComments.js
+++ app/components/ui/CardComments.js
@@ -32,29 +32,6 @@ export default function CardComments({ userName, datePosted, comment }) {
           </div>
         </footer>
         <p className="text-gray-500">{comment}</p>
-        <div className="flex items-center mt-4 space-x-4">
-          <button
-            type="button"
-            className="flex items-center text-sm text-gray-500 hover:underline font-medium"
-          >
-            <svg
-              className="mr-1.5 w-3.5 h-3.5"
-              aria-hidden="true"
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 20 18"
-            >
-              <path
-                stroke="currentColor"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M5 5h5M5 8h2m6-3h2m-5 3h6m2-7H2a1 1 0 0 0-1 1v9a1 1 0 0 0 1 1h3v5l5-5h8a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1Z"
-              />
-            </svg>
-            Reply
-          </button>
-        </div>
       </article>
       <div className="mx-6 border-b border-gray-200"></div>
     </>
